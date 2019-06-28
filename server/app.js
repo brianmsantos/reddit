034526b47b600534302e50cb6ec7004f776fb1c8
Brian Santos <brianmsantos81@gmail.com>
2019-06-27 20:51:21 -0700

    routes and controller added

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 //Require express 
 import express from 'express';
-
+import mongoose from 'mongoose';
 import routes from './routes';
+
+mongoose.connect('mongodb://localhost:27017/reddit', () =>{
+    console.log('connected to mongodb...');
+});
 
 const app = express();
 
