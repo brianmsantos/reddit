e3a555018d773332cb9c5071fb2bffb798f26998
Brian Santos <brianmsantos81@gmail.com>
2019-07-01 16:03:14 -0700

    user route setup in controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,11 +2,12 @@ import db from './../models';
 
 const userController = {};
 
+//Pulling out username and password from our response, req body
 userController.post = (req, res) => {
     const{ username, password} = req.body;
 
     //Validation
-
+    //Making a new instance of user
     const user = new db.User({
         username,
         password,
--- server/routes.js
+++ server/routes.js
@@ -2,9 +2,14 @@ import express from 'express';
 
 //Controller Imports
 import basicController from './controllers/basicController';
+import userController from './controllers/userController';
 
 const routes = express();
 
+//Basic routes
 routes.get('/', basicController.get) 
+
+//User routes
+routes.post('/signup', userController.post);
 
 export default routes;
